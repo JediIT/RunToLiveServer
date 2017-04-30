6c499235eaf3890ae5f88ac72f704bf0363edbb1
LENOVO-PC\bioss <bezvra@gmail.com>
2017-04-30 15:25:13 +0300

    - переделал тип конекта к базе на pool

--- api/points.js
+++ api/points.js
@@ -1,42 +1,72 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
-var pg = require("pg");
-var conString = "pg://user:password@localhost:5432/example";
-var client = new pg.Client(conString);
+var Pool = require('pg').Pool;
+
+// var pool = new Pool({
+//   host: 'localhost',
+//   user: 'osm',
+//   password: 'asdasdasd',
+//   database: 'test',
+//   max: 500, // max number of clients in the pool
+//   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+// });
+
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({
   extended: true
 }));
-client.connect();
+
+router.get('/get',function(req,res){
+
+  res.json(["one","2"]);
+  res.end();
+});
+
+router.post('/test',function(req,res){
+    var query = pool.query('SELECT * FROM public."Emergency";',  function (err, result) {
+    if(err) {
+      console.log(err);
+      pool.end();
+      return;
+    }
+    console.log(result.rows);
+
+    res.json(result.rows);
+    pool.end();
+    res.status(200);
+    res.end();
+  });
+});
 
 router.post('/ping',function(req,res){
-  var query = client.query('SELECT id, ST_AsText(location), type_id FROM public."Events" e WHERE ST_DWithin(e."location", ST_SetSRID(ST_MakePoint('+req.body.lat+','+req.body.lon+'), 4326),'+req.body.radius+') ORDER BY ST_Distance(e."location", ST_SetSRID(ST_Point('+req.body.lat+','+req.body.lon+'),4326));', function(err, result) {
-      if (err) {
-          console.log(err);
-          client.end();
-      }
-  });
+    var query = pool.query('SELECT id, ST_AsText(location), type_id FROM public."Events" e WHERE ST_DWithin(e."location", ST_SetSRID(ST_MakePoint('+req.body.lat+','+req.body.lon+'), 4326),'+req.body.radius+') ORDER BY ST_Distance(e."location", ST_SetSRID(ST_Point('+req.body.lat+','+req.body.lon+'),4326));',  function (err, result) {
+    if(err) {
+      console.log(err);
+      pool.end();
+      return;
+    }
+    console.log(result.rows);
 
-  query.on("end", function (result) {
     res.json(result.rows);
-    client.end();
+    pool.end();
     res.status(200);
     res.end();
   });
 });
 
 router.post('/add',function(req,res){
-  var query = client.query('INSERT INTO public."Events"(user_id, type_id, location, radius) VALUES ('+req.body.userId+', '+req.body.typeId+', ST_SetSRID(ST_MakePoint('+req.body.lat+','+req.body.lon+'), 4326), '+req.body.radius+');', function(err, result) {
-      if (err) {
-          console.log(err);
-          client.end();
-      }
-  });
+    var query = pool.query('INSERT INTO public."Events"(user_id, type_id, location, radius) VALUES ('+req.body.userId+', '+req.body.typeId+', ST_SetSRID(ST_MakePoint('+req.body.lat+','+req.body.lon+'), 4326), '+req.body.radius+');',  function (err, result) {
+    if(err) {
+      console.log(err);
+      pool.end();
+      return;
+    }
+    console.log(result.rows);
 
-  query.on("end", function (result) {
-    client.end();
+    res.json(result.rows);
+    pool.end();
     res.status(200);
     res.end();
   });
