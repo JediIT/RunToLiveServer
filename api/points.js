64f6d8e065443ed991cc068289bdc050cb087a7a
LENOVO-PC\bioss <bezvra@gmail.com>
2017-04-30 13:41:10 +0300

    - Добавил запрос на получение списка точек - Добавил запрос на добавление точек

--- api/points.js
+++ api/points.js
@@ -1,37 +1,43 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
-var Pool = require('pg').Pool;
+var pg = require("pg");
+var conString = "pg://user:password@localhost:5432/example";
 
-// var pool = new Pool({
-//   host: 'localhost',
-//   user: 'osm',
-//   password: 'asdasdasd',
-//   database: 'test',
-//   max: 500, // max number of clients in the pool
-//   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-// });
+var client = new pg.Client(conString);
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({
   extended: true
 }));
+client.connect();
 
 router.get('/get',function(req,res){
+  var query = client.query('SELECT id, ST_AsText(location), type_id FROM public."Events" e WHERE ST_DWithin(e."location", ST_SetSRID(ST_MakePoint('+req.body.lat+','+req.body.lon+'), 4326),'+req.body.radius+') ORDER BY ST_Distance(e."location", ST_SetSRID(ST_Point('+req.body.lat+','+req.body.lon+'),4326));', function(err, result) {
+      if (err) {
+          console.log(err);
+          client.end();
+      }
+  });
 
-  res.json(["one","2"]);
-  res.end();
+  query.on("end", function (result) {
+    res.json(result.rows);
+    client.end();
+    res.status(200);
+    res.end();
+  });
 });
 
 router.post('/add',function(req,res){
-  var query = pool.query("SELECT * FROM Emergency",  function (err, result) {
-    if(err) {
-    pool.end();
-    return;
-    }
+  var query = client.query('INSERT INTO public."Events"(user_id, type_id, location, radius) VALUES ('+req.body.userId+', '+req.body.typeId+', ST_SetSRID(ST_MakePoint('+req.body.lat+','+req.body.lon+'), 4326), '+req.body.radius+');', function(err, result) {
+      if (err) {
+          console.log(err);
+          client.end();
+      }
+  });
 
-    res.json(result.number);
-    pool.end();
+  query.on("end", function (result) {
+    client.end();
     res.status(200);
     res.end();
   });
